1e3df7971844887f49b4eaaad889337423f529e3
sheikhtaj466-netizen <sheikhtaj466@gmail.com>
2025-11-11 17:12:39 +0530

    Fix: Reverting static path to original

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const Message = mongoose.model('Message', messageSchema);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const protectRoute = (req, res, next) => {
   const token = req.cookies.token;
